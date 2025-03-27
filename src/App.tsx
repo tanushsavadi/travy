4052f137a8de98dd7b2682e51d397feed5b97646
Espiobest <59823894+Espiobest@users.noreply.github.com>
2025-03-27 13:20:38 -0400

    fix: remove create post from home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import { useState } from 'react'
-import CreatePost from './CreatePost'
 import Home from './pages/Home'
 import './App.css'
 
 function App() {
-
-  const [showCreatePost, setShowCreatePost] = useState(false)
-
-  const handleShowCreatePost = () => {
-    setShowCreatePost(!showCreatePost)
-  }
-
   return (
     <>
       <Home />
-      <div className="card">
-        <button onClick={handleShowCreatePost}>Create Post</button>
-      </div>
-      {showCreatePost && <CreatePost onClose={handleShowCreatePost} />}
-
     </>
   )
 }
